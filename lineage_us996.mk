60e6db0d211ad214a6d285e09fef01e9f9bd88ad
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    us996: Adjust override prop key names
    Change-Id: I92d64c23729494a14dd8d5de27ea7d65b78f42a5

--- lineage_us996.mk
+++ lineage_us996.mk
@@ -32,9 +32,7 @@ PRODUCT_MODEL := LG-US996
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="elsa" \
-    PRODUCT_DEVICE="elsa" \
-    PRODUCT_NAME="elsa_nao_us" \
-    PRIVATE_BUILD_DESC="elsa_nao_us-user 8.0.0 OPR1.170623.032 183241436cd40 release-keys"
-
-BUILD_FINGERPRINT := "lge/elsa_nao_us/elsa:8.0.0/OPR1.170623.032/183241436cd40:user/release-keys"
+    BuildDesc="elsa_nao_us-user 8.0.0 OPR1.170623.032 183241436cd40 release-keys" \
+    BuildFingerprint="lge/elsa_nao_us/elsa:8.0.0/OPR1.170623.032/183241436cd40:user/release-keys" \
+    DeviceName="elsa_nao_us" \
+    DeviceProduct="elsa"
